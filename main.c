5a78c09578bbde105b3dfd61a87a7fdb3b8ce49b
Etrunon <bosottiluca@gmail.com>
2015-04-30 11:52:11 +0200

    Init

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: etrunon
+ *
+ * Created on April 30, 2015, 11:35 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
