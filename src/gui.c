e7a96dbdeab25927db7f28e6aeb9be91596850b7
mion00 <mion00@gmail.com>
2015-05-15 11:53:46 +0200

    Primi test su GUI

---
+++ src/gui.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int punti[5] = {1, 5, 6, 8, 9};
+
+void printScreen() {
+    int i = 0;
+
+    int nonStampa[5];
+
+    int numero = 10;
+
+    for (i = 0; i < 20; i++) {
+        printf("%5c%5c", '#', '#');
+        printf("\n");
+        numero--;
+    }
+    //printf("\n");
+
+}
---
+++ src/gui.h
@@ -0,0 +1,13 @@
+/*
+ * File:   gui.h
+ * Author: mion00
+ *
+ * Created on May 14, 2015, 9:51 PM
+ */
+
+#ifndef GUI_H
+#define	GUI_H
+
+void printScreen();
+#endif	/* GUI_H */
+
